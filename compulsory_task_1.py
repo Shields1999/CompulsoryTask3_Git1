5a98a3a3860a3a916983a7529e6ecc361093197a
Shields1999 <125651889+Shields1999@users.noreply.github.com>
2024-08-19 08:13:11 +0100

    DesignGuide
    Brought the code in line with Style Guide, ensuring space and line length weren't too long

--- compulsory_task_1.py
+++ compulsory_task_1.py
@@ -1,31 +1,38 @@
 # create Class and Sub-Class
 class Course:
+    #name, contact_website can be changed to suit
     name = "Fundamentals of Computer Science"
     contact_website = "www.hyperiondev.com"
     location = "Cape Town"
 
     def contact_details(self):
-        print(f'''\nPlease contact us by visiting {self.contact_website}''')
+        print(f'''
+Please contact us by visiting {self.contact_website}''')
         
     def location(self):
-        print(f'''\nOur Head Office is based in {self.location}''')
+        print(f'''
+Our Head Office is based in {self.location}''')
 
+#Create Sub-Class
 class OOPCourse(Course):
     
+    #description, trainer and course_id can be changed to suit
     description = "OOP Fundamentals"
     trainer = "Mr Anon A. Mouse"
     course_id = "#12345"
         
     def trainer_details(self):
-        print(f'''\nThis course is the {self.name} which focuses
+        print(f'''
+This course is the {self.name} which focuses
 on {self.description} lead by {self.trainer}''')
         
     def show_course_id(self):
-        print(f"\nThe ID for the course is: {self.course_id}")
+        print(f"The ID for the course is: {self.course_id}\n")
         
-
+#Displays course defaults inside terminal
 course_1 = OOPCourse()
 
+#Displays course specific information inside terminal
 course_1.contact_details()
 course_1.trainer_details()
 course_1.show_course_id()
